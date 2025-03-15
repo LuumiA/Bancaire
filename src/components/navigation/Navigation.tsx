162e7d7a929b02352de67b5066cf8873d64f83a4
LuumiA <nicolas.cava13@gmail.com>
2025-03-15 15:12:38 +0100

    Appele des element du store avec dispatch , et utilisation des variables d'etat

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import logo from "../../assets/img/argentBankLogo.png";
 import { NavLink } from "react-router-dom";
-// import { AppDispatch, RootState } from "../redux/store";
-// import { logout } from "../redux/authSlice";
+import { AppDispatch } from "../../redux/store";
+import { logout } from "../../redux/feature/authSlices";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  selectIsAuthenticated,
+  selectUser,
+} from "../../redux/feature/authSelector";
+
 const Navigation: React.FC = () => {
-  // const dispatch: AppDispatch = useDispatch();
+  const dispatch: AppDispatch = useDispatch();
   // We retrieve the authentication status and user profile from the Redux store
-  // const { isAuthenticated, userProfile } = useSelector(
-  //   (state: RootState) => state.auth
-  // );
-  // const [userName, setUserName] = useState("");
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const user = useSelector(selectUser);
+  const [userProfileName, setUserProfileName] = useState("");
   // We handle the logout action by dispatching the logout function
-  // const handleLogout = () => {
-  //   dispatch(logout());
-  // };
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   // Effect to update the userName when the user profile is updated
-  // useMemo(() => {
-  //   if (isAuthenticated) {
-  //     setUserName(userProfile?.userName || "");
-  //   }
-  // }, [isAuthenticated, userProfile?.userName]);
+  useMemo(() => {
+    if (isAuthenticated) {
+      setUserProfileName(user?.userName || "");
+    }
+  }, [isAuthenticated, user?.userName]);
+
   return (
     <header>
       <nav className="main-nav">
@@ -34,21 +42,23 @@ const Navigation: React.FC = () => {
         </NavLink>
 
         <div>
-          <>
-            <NavLink className="main-nav-item" to="/profile">
+          {isAuthenticated ? (
+            <>
+              <NavLink className="main-nav-item" to="/profile">
+                {userProfileName}
+                <i className="fa fa-user-circle"></i>
+              </NavLink>
+              <NavLink to="/" className="main-nav-item" onClick={handleLogout}>
+                <i className="fa fa-sign-out"></i>
+                Sign Out
+              </NavLink>
+            </>
+          ) : (
+            <NavLink to="/login" className="main-nav-item">
               <i className="fa fa-user-circle"></i>
-              Hello
+              Sign In
             </NavLink>
-            <NavLink to="/" className="main-nav-item">
-              <i className="fa fa-sign-out"></i>
-              Sign Out
-            </NavLink>
-          </>
-
-          <NavLink to="/login" className="main-nav-item">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </NavLink>
+          )}
         </div>
       </nav>
     </header>
