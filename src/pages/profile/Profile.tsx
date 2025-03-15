24359bc0bc356fc2dd39038473519f14d4e1c791
LuumiA <nicolas.cava13@gmail.com>
2025-03-15 15:08:58 +0100

    Appel des api avec dispatch,ajout de condition pour afficher le composan editUser

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "../../components/navigation/Navigation";
 import Footer from "../../components/footer/Footer";
 import Account from "../../components/account/Account";
 import ProfileModif from "../../components/profilModif/ProfilModif";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../redux/store";
+import { fetchUserProfile } from "../../redux/feature/authActions";
+import { EditUserInfo } from "../../components/editUserInfo/EditUserInfo";
 // Données des comptes
 const accountsData = [
   {
@@ -24,12 +28,41 @@ const accountsData = [
     description: "Current Balance",
   },
 ];
+
 const Profile: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  // We retrieve the user profile and profile edit state from the Redux store
+  const { isEditingProfile, user, error, loading } = useSelector(
+    (state: RootState) => state.auth
+  );
+  // We set local state for displaying user information
+  const [userName, setUserName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  // Effect to fetch user profile when the component mounts
+  useEffect(() => {
+    // We dispatch fetchUserProfile to load the profile on startup
+    dispatch(fetchUserProfile());
+  }, [dispatch]);
+  // Effect to update local fields when the userProfile changes
+  useEffect(() => {
+    if (user) {
+      setUserName(user.userName || "");
+      setFirstName(user.firstName || "");
+    }
+  }, [user]);
+
+  if (loading) return <p>Loading accounts...</p>;
+  if (error) return <p>Error loading accounts: {error}</p>;
+
   return (
     <>
       <Navigation />
       <main className="main bg-dark">
-        <ProfileModif userName="Juko" firstName="Nicolas" />
+        {isEditingProfile ? (
+          <EditUserInfo />
+        ) : (
+          <ProfileModif userName={userName} firstName={firstName} />
+        )}
         <section>
           <h2 className="sr-only">Accounts</h2>
           <Account accounts={accountsData} />
