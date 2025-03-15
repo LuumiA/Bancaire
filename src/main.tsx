4f7f385f0d8f7d9a2fdabec7c0bd7d3b550f7192
LuumiA <nicolas.cava13@gmail.com>
2025-03-15 15:29:22 +0100

    Configuration du composant principal avec persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
 import { App } from "./App.tsx";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store.ts";
+import "font-awesome/css/font-awesome.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
