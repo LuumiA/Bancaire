3d9f55b84b19cd81e98b54df6df5ed65ed79cbb2
LuumiA <nicolas.cava13@gmail.com>
2025-03-21 21:19:47 +0100

    Modification des props pour faire la boucle dans le coposant parent

--- src/components/account/Account.tsx
+++ src/components/account/Account.tsx
@@ -8,23 +8,21 @@ interface AccountData {
 }
 // Déclaration des props attendues par le composant
 interface AccountProps {
-  accounts: AccountData[]; // Le composant reçoit un tableau de comptes
+  account: AccountData; // Le composant reçoit un tableau de comptes
 }
-const Account: React.FC<AccountProps> = ({ accounts }) => {
+const Account: React.FC<AccountProps> = ({ account }) => {
   return (
     <>
-      {accounts.map((account) => (
-        <div key={account.id} className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">{account.title}</h3>
-            <p className="account-amount">{account.amount}</p>
-            <p className="account-amount-description">{account.description}</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
+      <div key={account.id} className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">{account.title}</h3>
+          <p className="account-amount">{account.amount}</p>
+          <p className="account-amount-description">{account.description}</p>
         </div>
-      ))}
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </div>
     </>
   );
 };
